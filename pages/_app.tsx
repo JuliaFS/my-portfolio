9d07bcef2198c8925831462644a940aa293ff679
Yulia <yuliya.f.s@gmail.com>
2025-09-05 19:21:48 +0300

    test control head from rotating in small screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
+import Head from 'next/head'
 // import FlyingEffect from '../components/FlyingEffect'
 // import DrugTrail from '../components/DrugTrail'
 
@@ -9,6 +10,13 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
+            <Head>
+        {/* Add your viewport meta here */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <Component {...pageProps} />
       
       {/* <DrugTrail /> */}
