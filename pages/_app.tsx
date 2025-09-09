22bb603831007a544ea861921b1a139ce11ef83c
Yulia <yuliya.f.s@gmail.com>
2025-09-09 12:41:18 +0300

    add favicon and page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,9 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
       
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
-// pages/index.tsx
-import HeroSection from "../components/HeroSection/HeroSection";
+
+
+import { Metadata } from 'next';
+import HeroSection from '../components/HeroSection/HeroSection';
+
+export const metadata: Metadata = {
+  title: 'My Portfolio - Home',
+  description: 'A showcase of my projects and skills.',
+};
 
 export default function HomePage() {
-  return <HeroSection />;
+  return (
+    <HeroSection />
+  );
 }
-
